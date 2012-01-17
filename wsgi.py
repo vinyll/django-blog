7affcd6610b660b855c9c42707df5840366c3bcc
Vincent Agnano <vincent.agnano@particul.es>
2012-01-17 15:28:51 +0100

    updated wsgi for prod

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,11 @@
 import os, sys
 
-sys.path.append(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'venv', 'lib', 'python2.6', 'site-packages'))
+PROJECT_ROOT = sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+sys.path.append(
+    os.path.join(PROJECT_ROOT, 'venv', 'lib', 'python2.6', 'site-packages'),
+    PROJECT_ROOT,
+    )
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "conf.settings")
 
