2b3d8265376fa1c1ee343eb51e6752860757312e
Vincent Agnano <vincent.agnano@particul.es>
2012-01-17 15:44:51 +0100

    updated wsgi for prod

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os, sys
 
-PROJECT_ROOT = sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 sys.path.append(os.path.join(PROJECT_ROOT, 'venv', 'lib', 'python2.6', 'site-packages'))
 sys.path.append(PROJECT_ROOT)
